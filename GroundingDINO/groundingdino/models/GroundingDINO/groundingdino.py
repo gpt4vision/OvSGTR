fde404e0b88c09347872d6ed2ea5a0418f7f79e5
Joseph <zychen.uestc@gmail.com>
2024-09-14 11:27:17 +0000

    update license

--- GroundingDINO/groundingdino/models/GroundingDINO/groundingdino.py
+++ GroundingDINO/groundingdino/models/GroundingDINO/groundingdino.py
@@ -1,5 +1,12 @@
 # ------------------------------------------------------------------------
 # modified from Grounding DINO
+# url: https://github.com/gpt4vision/OvSGTR
+# Copyright (c) 2024 Joseph Chen. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# 
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# Grounding DINO
 # url: https://github.com/IDEA-Research/GroundingDINO
 # Copyright (c) 2023 IDEA. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 [see LICENSE for details]
